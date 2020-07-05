7453411344379929fe84522ade00b1631b16e640
yoohooitstoo <stumanscott@gmail.com>
2020-07-05 15:10:12 -0400

    view route to notes.html works

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ const fs = ("fs");
 const app = express()
 // 3. Create a PORT
 const PORT = process.env.PORT || 3000;
-// 4. Listen on that port
-app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-});
 
 
 // View Routes -> HTML
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./02-Homework/Develop/public/index.html"))
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./02-Homework/Develop/public/notes.html"))
+});
+
+
+// 4. Listen on that port
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
 });
