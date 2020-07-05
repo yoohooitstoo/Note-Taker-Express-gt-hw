649db5ab6c012a0cbddc2fa6376d31a16b125b6c
yoohooitstoo <stumanscott@gmail.com>
2020-07-05 16:24:43 -0400

    added in a GET route for api

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./02-Homework/Develop/public/notes.html"))
 });
 
+// API Routes -> JSON
+app.get("api/notes", (req, res) => {
+    fs.readFile("./02-Homework/Develop/db/db.json", "utf8", (err,data) => {
+        if (err) {
+            retun res.send("An error occured reading you data");
+        }
+        const arrayofNotes = JSON.parse(data);
+        res.json(arrayofNotes);
+    });
+});
+
 
 // 4. Listen on that port
 app.listen(PORT, () => {
