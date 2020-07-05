7dedaef1d0e65b58a60e7d7eb8f793e7d3d3c6a2
yoohooitstoo <stumanscott@gmail.com>
2020-07-05 15:06:59 -0400

    first view route to index.html works

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // 1. Require Express
 const express = require("express");
 const path = require("path");
-const e = require("express");
-conat fs = ("fs");
+const fs = ("fs");
 
 // 2. Create an instance of Express - app
 const app = express()
@@ -11,4 +10,10 @@ const PORT = process.env.PORT || 3000;
 // 4. Listen on that port
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
+});
+
+
+// View Routes -> HTML
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./02-Homework/Develop/public/index.html"))
 });
